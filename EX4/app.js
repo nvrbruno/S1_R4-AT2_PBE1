eb17d50ea5ba16776648b62fcef107b4cd6aa30d
unknown <brunonavarrodavi@gmail.com>
2025-09-02 12:50:37 -0300

    ATIVIDADE BACKEND

--- EX4/app.js
+++ EX4/app.js
@@ -6,26 +6,29 @@ const PORT = 8081; // Configura a porta 8081 como a porta para comunicação
 const fs = require("fs");
 
 app.get("/livros", (req, res) => {
-  const { titulo, autor, data } = req.query; 
+  const { titulo, autor, data } = req.query;
   try {
     const arquivoData = fs.readFileSync("./livros.json", "utf-8");
     let livros = JSON.parse(arquivoData);
 
-    if (titulo) { //Cria um filtro onde ele procura pelo nome enviado pelo usuario.
+    if (titulo) {
+      //Cria um filtro onde ele procura pelo nome enviado pelo usuario.
       livros = livros.filter((livro) =>
         livro.titulo.toLowerCase().includes(titulo.toLowerCase())
       ); // Transforma tudo em letra minuscula
     }
 
     if (autor) {
-      livros = livros.filter((livro) =>  //Cria um filtro onde ele procura pelo nome enviado pelo usuario.
-        livro.autor.toLowerCase().includes(autor.toLowerCase())
-      );// Transforma tudo em letra minuscula
+      livros = livros.filter(
+        (
+          livro //Cria um filtro onde ele procura pelo nome enviado pelo usuario.
+        ) => livro.autor.toLowerCase().includes(autor.toLowerCase())
+      ); // Transforma tudo em letra minuscula
     }
 
     if (data) {
       livros = livros.filter((livro) => livro.data <= data);
-    }// Função que procura o envento de acordo com a data (minima)
+    } // Função que procura o envento de acordo com a data (minima)
 
     res.status(200).json(livros);
   } catch (error) {
